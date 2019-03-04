8b2f459fc910cc06bd222e6a3f1774831260964a
tony30337@gatech.edu <tony30337@gatech.edu>
2019-03-03 20:32:01 -0500

    Added structure design, need to connect to backend

---
+++ CareConnectGiver/CareConnectGiver/PeopleDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PeopleDetailViewController.swift
+//  CareConnectGiver
+//
+//  Created by Tony on 3/3/19.
+//  Copyright © 2019 Tony. All rights reserved.
+//
+
+import UIKit
+
+class PeopleDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cancel(segue:UIStoryboardSegue) {
+        
+    }
+    
+    @IBAction func done(segue:UIStoryboardSegue) {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
