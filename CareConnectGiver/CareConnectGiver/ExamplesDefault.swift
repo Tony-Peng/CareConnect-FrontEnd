70618c5a44c8ee5f2bd26f447ce5eb9307c494d0
michaelchen523 <michaelchen523@gmail.com>
2019-04-14 22:21:52 -0400

    Mood chart in giver app

---
+++ CareConnectGiver/CareConnectGiver/ExamplesDefault.swift
@@ -0,0 +1,105 @@
+//
+//  ExamplesDefault.swift
+//  CareConnectGiver
+//
+//  Created by Michael Chen on 4/14/19.
+//  Copyright © 2019 Tony. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SwiftCharts
+
+struct ExamplesDefaults {
+    
+    static var chartSettings: ChartSettings {
+        return iPhoneChartSettings
+    }
+    
+    static var chartSettings2: ChartSettings {
+        return iPhoneChartSettings
+    }
+    
+    static var chartSettingsWithPanZoom: ChartSettings {
+        return iPhoneChartSettingsWithPanZoom
+    }
+    
+    static var chartSettingsWithPanZoom2: ChartSettings {
+        return iPhoneChartSettingsWithPanZoom2
+    }
+    
+    fileprivate static var iPhoneChartSettings: ChartSettings {
+        var chartSettings = ChartSettings()
+        chartSettings.leading = 10
+        chartSettings.top = 10
+        chartSettings.trailing = 10
+        chartSettings.bottom = 10
+        chartSettings.labelsToAxisSpacingX = 5
+        chartSettings.labelsToAxisSpacingY = 5
+        chartSettings.axisTitleLabelsToLabelsSpacing = 4
+        chartSettings.axisStrokeWidth = 0.2
+        chartSettings.spacingBetweenAxesX = 8
+        chartSettings.spacingBetweenAxesY = 8
+        chartSettings.labelsSpacing = 0
+        return chartSettings
+    }
+    
+    fileprivate static var iPhoneChartSettings2: ChartSettings {
+        var chartSettings = ChartSettings()
+        chartSettings.leading = 0
+        chartSettings.top = 0
+        chartSettings.trailing = 0
+        chartSettings.bottom = 0
+        chartSettings.labelsToAxisSpacingX = 0
+        chartSettings.labelsToAxisSpacingY = 0
+        chartSettings.axisTitleLabelsToLabelsSpacing = 0
+        chartSettings.axisStrokeWidth = 0.0
+        chartSettings.spacingBetweenAxesX = 0
+        chartSettings.spacingBetweenAxesY = 0
+        chartSettings.labelsSpacing = 0
+        return chartSettings
+    }
+    
+    fileprivate static var iPhoneChartSettingsWithPanZoom: ChartSettings {
+        var chartSettings = iPhoneChartSettings
+        chartSettings.zoomPan.panEnabled = true
+        chartSettings.zoomPan.zoomEnabled = true
+        return chartSettings
+    }
+    
+    fileprivate static var iPhoneChartSettingsWithPanZoom2: ChartSettings {
+        var chartSettings = iPhoneChartSettings2
+        chartSettings.zoomPan.panEnabled = true
+        chartSettings.zoomPan.zoomEnabled = true
+        return chartSettings
+    }
+    
+    static func chartFrame(_ containerBounds: CGRect) -> CGRect {
+        return CGRect(x: 30
+            , y: 115, width: 295, height: 105)
+    }
+    
+    static var labelSettings: ChartLabelSettings {
+        return ChartLabelSettings(font: ExamplesDefaults.labelFont)
+    }
+    
+    static var labelFont: UIFont {
+        return ExamplesDefaults.fontWithSize(11)
+    }
+    
+    static var labelFontSmall: UIFont {
+        return ExamplesDefaults.fontWithSize(10)
+    }
+    
+    static func fontWithSize(_ size: CGFloat) -> UIFont {
+        return UIFont(name: "Helvetica", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
+    static var guidelinesWidth: CGFloat {
+        return 0.1
+    }
+    
+    static var minBarSpacing: CGFloat {
+        return 5
+    }
+}
--- CareConnectGiver/CareConnectGiver/PeopleDetailViewController.swift
+++ CareConnectGiver/CareConnectGiver/PeopleDetailViewController.swift
@@ -9,21 +9,112 @@
 import UIKit
 import Alamofire
 import SwiftyJSON
+import SwiftCharts
 
 class PeopleDetailViewController: UIViewController {
     
     var person: Person?
+    fileprivate var barChart: Chart?
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var profilePic: UIImageView!
+    @IBOutlet weak var moodLabel: UILabel!
     
-    
+    var moodResponse = [(date: String, val: Int)]()
+    let dateFormatter = DateFormatter()
+    let dateFormatterString = DateFormatter()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.title = self.person?.name
         self.titleLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        moodLabel.font = UIFont.boldSystemFont(ofSize: 17)
         self.loadProfilePicture()
+        //Grab the survey result from API
+        let url = "https://mas-care-connect.herokuapp.com/quizresponses/?elderly=" + String(person!.id)
+        Alamofire.request(url, method: .get).responseJSON { response in
+            switch response.result {
+            case .success(let result):
+                self.dateFormatter.dateFormat = "yyyy-MM-dd"
+                self.dateFormatterString.dateFormat = "M/d"
+                let json = JSON(result)
+                for(_,v) in json {
+                    let date = self.dateFormatter.date(from: String(v["date"].string!.prefix(10)))
+                    let dateString = self.dateFormatterString.string(from: date!)
+                    self.moodResponse.append((date: dateString, val: v["q8_mood"].int!))
+                }
+                
+                let moodData = self.countMood(input: self.moodResponse.prefix(7))
+                self.generateImageBarChart(xValue: 50, yValue: 260, moodData: moodData)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func generateImageBarChart(xValue: Int, yValue: Int, moodData: [(mood: String, value: Int)]) {
+        let labelSettings = ChartLabelSettings(font: ExamplesDefaults.fontWithSize(0))
+        let moodAxisValues = [ChartAxisValueString(order: -1)] +
+            moodData.enumerated().map {index, tuple in ChartAxisValueString(String(tuple.0), order: index, labelSettings: labelSettings)} + [ChartAxisValueString(order: moodData.count)]
+        
+        let valueAxisValues = [ChartAxisValueString(order: -1)] +
+            moodData.enumerated().map {index, tuple in ChartAxisValueString(String(tuple.1), order: index, labelSettings: labelSettings)} + [ChartAxisValueString(order: moodData.count)]
+        
+        let xModel = ChartAxisModel(axisValues: valueAxisValues, axisTitleLabel: ChartAxisLabel(text: "Value", settings: labelSettings))
+        let yModel = ChartAxisModel(axisValues: moodAxisValues, axisTitleLabel: ChartAxisLabel(text: "Mood", settings: labelSettings.defaultVertical()))
+        let chartFrame = CGRect(x: xValue
+            , y: yValue, width: 340, height: 240)
+        
+        let chartSettings = ExamplesDefaults.chartSettingsWithPanZoom2
+        
+        let coordsSpace = ChartCoordsSpaceLeftBottomSingleAxis(chartSettings: chartSettings, chartFrame: chartFrame, xModel: xModel, yModel: yModel)
+        let (xAxisLayer, yAxisLayer, innerFrame) = (coordsSpace.xAxisLayer, coordsSpace.yAxisLayer, coordsSpace.chartInnerFrame)
+        let barViewSettings = ChartBarViewSettings(animDuration: 0.5)
+        let zero = ChartAxisValueDouble(0)
+        let bars: [ChartBarModel] = moodData.enumerated().flatMap {tuple in
+            [
+                ChartBarModel(constant: ChartAxisValueDouble(tuple.offset), axisValue1: zero, axisValue2: ChartAxisValueDouble(tuple.element.value), bgColor: UIColor(
+                    red: CGFloat(0.0/255.0),
+                    green: CGFloat(122.0/255.0),
+                    blue: CGFloat(255.0/255.0),
+                    alpha: CGFloat(0.8)
+                ))
+            ]
+        }
+        let barsLayer = ChartBarsLayer(xAxis: xAxisLayer.axis, yAxis: yAxisLayer.axis, bars: bars, horizontal: true, barWidth: 20
+            , settings: barViewSettings)
+        let chart = Chart(
+            frame: chartFrame,
+            innerFrame: innerFrame,
+            settings: chartSettings,
+            layers: [
+                xAxisLayer,
+                yAxisLayer,
+                barsLayer
+            ]
+        )
+        self.view.addSubview(chart.view)
+        self.barChart = chart
+    }
+    
+    func countMood(input : ArraySlice<(date: String, val: Int)>)  -> [(mood: String, value: Int)]{
+        print(input)
+        var counts = [
+            0 : 0,
+            1 : 0, // Always use optional values carefully!
+            2 : 0,
+            3 : 0,
+            4 : 0
+        ]
+        for (_,v) in input {
+            counts.updateValue(counts[Int(v)]! + 1, forKey: Int(v))
+        }
+        var returnArray = [(mood: String, value: Int)]()
+        for (k,v) in counts {
+            returnArray.append((mood: String(k), value: v))
+        }
+        returnArray.sort() { $0.0 < $1.0 }
+        return returnArray
     }
     
     func loadProfilePicture() {
